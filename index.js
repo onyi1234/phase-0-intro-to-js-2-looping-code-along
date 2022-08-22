657c7a585ff0f51a0f233c53489c6af28d3da651
onyi1234 <onyangofrank30@gmail.com>
2022-08-22 08:01:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+
+
+    const messages = [];
+   
+     for (let i = 0; i < names.length; i++) {
+   
+       const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+   
+       
+       messages.push(message);
+     }
+   
+     return messages;
+   
+   }
+   
+   console.log(writeCards(['Guadalupe', 'Ollie', 'Aki'], 'wedding'));
+
+   function countDown(i){
+
+    let answer = '';
+    while(i>=0){;
+        answer = answer + i;
+       console.log(i--);
+    }
+    return answer;
+   }
+   countDown(10);
